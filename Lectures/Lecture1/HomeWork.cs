567b4456441000d8e952c9f2989128326c43800c
Serhii Shkodenko <serhii.shkodenko@altexsoft.com>
2021-04-09 23:09:03 +0300

    Homework for Lecture1 was added.

---
+++ Lectures/Lecture1/HomeWork.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+
+namespace Lecture1
+{
+    internal class HomeWork
+    {
+        private decimal GetFullPrice(
+                                    IEnumerable<string> destinations,
+                                    IEnumerable<string> clients,
+                                    IEnumerable<int> infantsIds,
+                                    IEnumerable<int> childrenIds,
+                                    IEnumerable<decimal> prices,
+                                    IEnumerable<string> currencies)
+        {
+            decimal fullPrice = default;
+
+            // Your solution supposed to be here.
+
+            return fullPrice;
+        }
+
+        public decimal InvokePriceCalculatiion()
+        {
+            var destinations = new[]
+            {
+                "949 Fairfield Court, Madison Heights, MI 48071",
+                "367 Wayne Street, Hendersonville, NC 28792",
+                "910 North Heather Street, Cocoa, FL 32927",
+                "911 North Heather Street, Cocoa, FL 32927",
+                "706 Tarkiln Hill Ave, Middleburg, FL 32068",
+            };
+
+            var clients = new[]
+            {
+                "Autumn Baldwin",
+                "Jorge Hoffman",
+                "Amiah Simmons",
+                "Sariah Bennett",
+                "Xavier Bowers",
+            };
+
+            var infantsIds = new[] { 2 };
+            var childrenIds = new[] { 3, 4 };
+
+            var prices = new[] { 100, 25.23m, 58, 23.12m, 125 };
+            var currencies = new[] { "USD", "USD", "EUR", "USD", "USD" };
+
+            return GetFullPrice(destinations, clients, infantsIds, childrenIds, prices, currencies);
+        }
+    }
+}
---
+++ Lectures/Lecture1/StackHeap.cs
@@ -0,0 +1,101 @@
+﻿using System;
+
+namespace Lecture1
+{
+    public class StackHeap
+    {
+        private void TypesMemoryHandling(ref int valueParam, object referenceParam)
+        {
+            var someValueParam = 5;
+            var someReferenceParam = "5";
+
+            Console.WriteLine();
+            Console.WriteLine("---Attempt to change the TestClass property value---");
+
+            valueParam = int.MaxValue;
+            referenceParam = Guid.NewGuid().ToString();
+
+            Console.WriteLine(valueParam);
+            Console.WriteLine(referenceParam);
+        }
+
+        private void TestClassMemoryHandling(TestClass entityToChange)
+        {
+            Console.WriteLine();
+            Console.WriteLine("---Attempt to change the TestClass property value---");
+
+            entityToChange.ValueToChange = int.MaxValue;
+
+            Console.WriteLine(entityToChange.ValueToChange);
+        }
+
+        public void Run()
+        {
+            var originalValueParam = 1000;
+            string originalReferenceParam = "1000";
+
+            Console.WriteLine();
+            Console.WriteLine("---Original values before the changing attempt---");
+            Console.WriteLine(originalValueParam);
+            Console.WriteLine(originalReferenceParam);
+
+            TypesMemoryHandling(ref originalValueParam, originalReferenceParam);
+
+            Console.WriteLine();
+            Console.WriteLine("---Original values after the changing attempt---");
+            Console.WriteLine(originalValueParam);
+            Console.WriteLine(originalReferenceParam);
+        }
+
+        public void RunTestClass()
+        {
+            var objectToTest = new TestClass();
+            objectToTest.ValueToChange = 5;
+
+            Console.WriteLine();
+            Console.WriteLine("---Original values before the changing attempt---");
+            Console.WriteLine(objectToTest.ValueToChange);
+
+            TestClassMemoryHandling(objectToTest);
+
+            Console.WriteLine();
+            Console.WriteLine("---Original values after the changing attempt---");
+            Console.WriteLine(objectToTest.ValueToChange);
+
+            //var valueToCheck = 5;
+
+            //if (valueToCheck == 5)
+            //{
+            //    Console.WriteLine("Value is 5.");
+            //}
+            //else if (valueToCheck == 4)
+            //{
+            //    Console.WriteLine("Value is 4.");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Value is not 5 or 4.");
+            //}
+
+            //var result = valueToCheck == 5
+            //    ? true
+            //    : false;
+
+            //int caseSwitch = 1;
+
+            //var result2 = caseSwitch switch
+            //{
+            //    1 => "someString",
+            //    2 => "someString2",
+            //    _ => throw new NotImplementedException(),
+            //};
+
+            
+        }
+
+        public class TestClass
+        {
+            public int ValueToChange { get; set; }
+        }
+    }
+}
