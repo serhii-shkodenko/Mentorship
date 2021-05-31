9e862f09324fe9e574427a7f4fe330fa7e6ad70b
Serhii Shkodenko <serhii.shkodenko@altexsoft.com>
2021-05-31 18:20:53 +0300

    Were added materials for Lecture 11.

---
+++ Lectures/Lecture11/Program.cs
@@ -0,0 +1,63 @@
+﻿using Lecture11;
+using static System.Console;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        RunSimleExample();
+        //ExampleWithIdenticalItems();
+        //ExampleGetByHash();
+
+        // General
+        ReadLine();
+    }
+
+    private static void RunSimleExample()
+    {
+        var featureToInit = new[]
+        {
+            "Feature one",
+            "Feature two",
+        };
+
+        var features = new ApplicationFeatures(featureToInit);
+        var enumerator = features.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            WriteLine(enumerator.Current);
+        }
+    }
+
+    private static void ExampleWithIdenticalItems()
+    {
+        var featureToInit = new[]
+        {
+            "Feature one",
+            "Feature one",
+        };
+
+        var features = new ApplicationFeatures(featureToInit);
+        var enumerator = features.GetEnumerator();
+
+        while (enumerator.MoveNext())
+        {
+            WriteLine(enumerator.Current);
+        }
+    }
+
+    private static void ExampleGetByHash()
+    {
+        var featureToInit = new[]
+        {
+            "Feature one",
+            "Feature two",
+        };
+
+        var features = new ApplicationFeatures(featureToInit);
+
+        //WriteLine(features[1]);
+        WriteLine(features["Feature two".GetHashCode()]);
+    }
+}
