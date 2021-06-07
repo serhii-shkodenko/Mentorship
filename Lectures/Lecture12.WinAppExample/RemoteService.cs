cbd312ce7ef28defeb52492209f573d600359dd4
Serhii Shkodenko <serhii.shkodenko@altexsoft.com>
2021-06-07 17:45:44 +0300

    Were added materials for the student's asynchronous programming learning task.

---
+++ Lectures/Lecture12.WinAppExample/RemoteService.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture12.WinAppExample
+{
+    public class RemoteService
+    {
+        public async Task GetDataAsync()
+        {
+            await Task.Delay(3000);
+            MessageBox.Show("Remote service returned data.");
+        }
+
+        public void GetData()
+        {
+            Thread.Sleep(3000);
+            MessageBox.Show("Remote service returned data.");
+        }
+
+        public async Task<string> GetPlaceHolderApiDataAsync()
+        {
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos");
+            var response = await client.SendAsync(request).ConfigureAwait(false);
+
+            Console.WriteLine("====== Inside the call ======");
+            Console.WriteLine(Thread.CurrentThread.Name);
+            Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+            Console.WriteLine($"ThreadCount: {ThreadPool.ThreadCount}");
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> GetPlaceHolderApiDataAsync(TaskScheduler scheduler, Action<string> perfomActionOverUiControl)
+        {
+            using var cancellationSource = new CancellationTokenSource();
+
+            var client = new HttpClient();
+            var request = new HttpRequestMessage(HttpMethod.Get, "https://jsonplaceholder.typicode.com/todos");
+
+            var sendingTask = Task.Factory.StartNew(() =>
+            {
+                Console.WriteLine("====== Inside the sending task ======");
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine($"ThreadCount: {ThreadPool.ThreadCount}");
+
+                return client.SendAsync(request);
+            }, cancellationSource.Token).Unwrap();
+
+            var getContentTask = sendingTask.ContinueWith((response) =>
+            {
+                Console.WriteLine("====== Inside the getting content task ======");
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine($"ThreadCount: {ThreadPool.ThreadCount}");
+
+                return response.GetAwaiter().GetResult().Content.ReadAsStringAsync();
+            }, cancellationSource.Token).Unwrap();
+
+            return await await getContentTask.ContinueWith(async (content, obj) =>
+            {
+                Console.WriteLine("====== Inside the TextBox manipulating task ======");
+                Console.WriteLine(Thread.CurrentThread.Name);
+                Console.WriteLine(Thread.CurrentThread.ManagedThreadId);
+                Console.WriteLine($"ThreadCount: {ThreadPool.ThreadCount}");
+
+                var text = await content;
+
+                perfomActionOverUiControl(text);
+
+                return text;
+            }, cancellationSource.Token, scheduler);
+        }
+
+        public ValueTask<int> GetStatusCode()
+        {
+            return ValueTask.FromResult(10);
+        }
+    }
+}
---
+++ Lectures/Lecture12.WinAppExample/SyncVsAsync.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lecture12.WinAppExample
+{
+    public partial class SyncVsAsync : Form
+    {
+        private readonly RemoteService _remoteService;
+
+        public SyncVsAsync()
+        {
+            InitializeComponent();
+            _remoteService = new RemoteService();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            _remoteService.GetData();
+            await UpdateText(label1);
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            await _remoteService.GetDataAsync();
+            await UpdateText(label2);
+        }
+
+        private async Task<Product<MySuperType>> UpdateText(Label label)
+        {
+            label.Text = "Retrieveing data";
+
+            for (var i = 0; i < 10; i++)
+            {
+                await Task.Delay(250);
+                label.Text += ".";
+            }
+
+            return new Product<MySuperType>();
+        }
+
+        public class Product<TType> where TType : class, new()
+        {
+            TType GetT => new TType();
+        }
+
+        public class MySuperType { }
+    }
+}
